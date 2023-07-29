dded397f49b806f9d3d3426d6fcaf97f5c5e86b1
tqk2811 <tqk2811@gmail.com>
2023-07-29 10:33:40 +0700

    fix missing namespace

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using $safeprojectname$.UI.ViewModels;
+using $safeprojectname$.UI.ViewModels.WindowViewModels;
 
 namespace $safeprojectname$.UI
 {
