9e0c31ce4d413fd25ade17bd892d2592caedfc4e
tqk2811 <tqk2811@gmail.com>
2023-11-27 17:28:18 +0700

    update write log

--- UI/ViewModels/WindowViewModels/MainWVM.cs
+++ UI/ViewModels/WindowViewModels/MainWVM.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using TqkLibrary.WpfUi;
 using TqkLibrary.WpfUi.ObservableCollection;
+using System.Runtime.CompilerServices;
+
 namespace $safeprojectname$.UI.ViewModels.WindowViewModels
 {
     class MainWVM : BaseVM
@@ -16,9 +18,19 @@ namespace $safeprojectname$.UI.ViewModels.WindowViewModels
 
 		private static readonly LimitObservableCollection<string> _Logs 
 			= new LimitObservableCollection<string>(() => Singleton.LogDir + $"\\{DateTime.Now:yyyy-MM-dd}.log");
-		public static void WriteLog(string log)
-		{
-			_Logs.Add($"{DateTime.Now:HH:mm:ss} {log}");
-		}
+		public static void WriteLog(string log, [CallerMemberName] string callFunction = null)
+        {
+            _Logs.Add($"{DateTime.Now:HH:mm:ss} [{callFunction}] {log}");
+        }
+        public static void WriteExceptionLog(string message, Exception ex, [CallerMemberName] string callFunction = null)
+        {
+            if (ex is AggregateException ae) ex = ae.InnerException;
+            _Logs.Add($"{DateTime.Now:HH:mm:ss} [{callFunction}] [{message}] {ex.GetType().FullName}: {ex.Message}, {ex.StackTrace}");
+        }
+        public static void WriteExceptionLog(Exception ex, [CallerMemberName] string callFunction = null)
+        {
+            if (ex is AggregateException ae) ex = ae.InnerException;
+            _Logs.Add($"{DateTime.Now:HH:mm:ss} [{callFunction}] {ex.GetType().FullName}: {ex.Message}, {ex.StackTrace}");
+        }
     }
 }
