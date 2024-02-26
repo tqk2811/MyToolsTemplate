cd74050493a096999e0419ac0bb2e6786d0424e2
tqk2811 <tqk2811@gmail.com>
2024-02-26 17:09:07 +0700

    update BaseVM

--- UI/ViewModels/BaseVM.cs
+++ UI/ViewModels/BaseVM.cs
@@ -1,11 +1,57 @@
 ﻿using $safeprojectname$.DataClass;
+using $safeprojectname$.UI.Commands;
 using TqkLibrary.WpfUi;
 
 namespace $safeprojectname$.UI.ViewModels
 {
     internal class BaseVM : BaseViewModel
     {
+        protected readonly ILogger _logger;
+        public BaseVM()
+        {
+            _logger = Singleton.ILoggerFactory.CreateLogger(this.GetType());
+            this.CopyTextCommand = new BaseCommand<string>(_CopyTextCommand);
+            this.LinkNavigateCommand = new BaseCommand<string>(_LinkNavigateCommand);
+        }
+        
         protected SettingData Setting { get { return Singleton.Setting.Data; } }
         protected void SaveSetting() => Singleton.Setting.TriggerSave();
+
+        
+        public BaseCommand<string> CopyTextCommand { get; }
+        void _CopyTextCommand(string text)
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                try
+                {
+                    Clipboard.SetText(text);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogErrorFunction(ex);
+                }
+            }
+        }
+
+        public BaseCommand<string> LinkNavigateCommand { get; }
+        void _LinkNavigateCommand(string link)
+        {
+            if (Uri.TryCreate(link, UriKind.Absolute, out Uri? uri))
+            {
+                try
+                {
+                    using Process? process = Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = uri.ToString(),
+                        UseShellExecute = true,
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogErrorFunction(ex);
+                }
+            }
+        }
     }
 }
