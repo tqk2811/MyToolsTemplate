b83b3088348a54f4e3db1cc8ca1ad03239303fac
tqk2811 <tqk2811@gmail.com>
2024-03-02 17:18:39 +0700

    add OpenFolderDialogCommand, OpenFileDialogCommand

---
+++ UI/ViewModels/Commands/OpenFileDialogCommand.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace $safeprojectname$.UI.ViewModels.Commands
+{
+    internal class OpenFileDialogCommand<TObject> : OpenFolderDialogCommand<TObject> where TObject : class
+    {
+        readonly string _filter;
+        public OpenFileDialogCommand(TObject @object, Expression<Func<TObject, string?>> expression, Action saveCallback, string filter)
+            : base(@object, expression, saveCallback)
+        {
+            if (string.IsNullOrWhiteSpace(filter)) throw new ArgumentNullException(nameof(filter));
+            this._filter = filter;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Multiselect = false,
+                RestoreDirectory = true,
+                Filter = _filter,
+                CheckFileExists = true,
+            };
+            openFileDialog.CustomPlaces = new List<FileDialogCustomPlace>()
+            {
+                new FileDialogCustomPlace(Singleton.ExeDir)
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Path = openFileDialog.FileName;
+            }
+        }
+    }
+    static class OpenFileDialogCommandExtension
+    {
+        public static OpenFileDialogCommand<T> OpenFileDialogCommand<T>(this T obj, Expression<Func<T, string?>> expression, Action saveCallback, string filter)
+            where T : class
+        {
+            return new OpenFileDialogCommand<T>(obj, expression, saveCallback, filter);
+        }
+    }
+}
---
+++ UI/ViewModels/Commands/OpenFolderDialogCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace $safeprojectname$.UI.ViewModels.Commands
+{
+    internal class OpenFolderDialogCommand<TObject> : BaseCommand, INotifyPropertyChanged where TObject : class
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyPropertyChange([CallerMemberName] string name = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected readonly TObject _object;
+        protected readonly Expression<Func<TObject, string?>> _expression;
+        protected readonly Action _saveCallback;
+        public OpenFolderDialogCommand(TObject @object, Expression<Func<TObject, string?>> expression, Action saveCallback)
+        {
+            this._object = @object ?? throw new ArgumentNullException(nameof(@object));
+            this._expression = expression ?? throw new ArgumentNullException(nameof(expression));
+            this._saveCallback = saveCallback ?? throw new ArgumentNullException(nameof(saveCallback));
+
+            MemberExpression? memberExpression = expression.Body as MemberExpression;
+            PropertyInfo? propertyInfo = memberExpression?.Member as PropertyInfo;
+            if (propertyInfo is null) throw new InvalidOperationException($"{nameof(expression)} invalid selector");
+            if (!propertyInfo.CanRead || !propertyInfo.CanWrite) throw new InvalidOperationException($"{nameof(expression)} invalid selector, must be get and set");
+        }
+
+        public string? Path
+        {
+            get { return _expression.Compile().Invoke(_object); }
+            set
+            {
+                var prop = (PropertyInfo)((MemberExpression)_expression.Body).Member;
+                prop.SetValue(_object, value);
+                NotifyPropertyChange();
+                _saveCallback.Invoke();
+            }
+        }
+        public override void Execute(object? parameter)
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog()
+            {
+                Multiselect = false,
+                RestoreDirectory = true,
+                IsFolderPicker = true,
+                EnsurePathExists = true,
+            };
+            commonOpenFileDialog.AddPlace(Singleton.ExeDir, Microsoft.WindowsAPICodePack.Shell.FileDialogAddPlaceLocation.Top);
+            if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                Path = commonOpenFileDialog.FileName;
+            }
+        }
+
+        public static implicit operator string?(OpenFolderDialogCommand<TObject> openFolderDialogCommand) => openFolderDialogCommand?.Path;
+    }
+    static class OpenFolderDialogCommandExtension
+    {
+        public static OpenFolderDialogCommand<T> OpenFolderDialogCommand<T>(this T obj, Expression<Func<T, string?>> expression, Action saveCallback)
+            where T : class
+        {
+            return new OpenFolderDialogCommand<T>(obj, expression, saveCallback);
+        }
+    }
+}
