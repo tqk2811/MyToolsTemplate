abb06e0f117600703ee66208ee9e3aef32a6213e
tqk2811 <tqk2811@gmail.com>
2024-03-09 16:49:48 +0700

    update BaseDataVM

--- UI/ViewModels/BaseDataVM.cs
+++ UI/ViewModels/BaseDataVM.cs
@@ -1,16 +1,27 @@
 using System;
+using TqkLibrary.WpfUi.ObservableCollections;
 
 namespace $safeprojectname$.UI.ViewModels
 {
-    internal class BaseDataVM<TData> : BaseVM
+    internal class BaseDataVM<TData> : BaseVM, IViewModel<TData> where TData : class
     {
-        protected readonly TData _data;
-        readonly Action _saveCallback;
-        protected void Save() => _saveCallback.Invoke();
-        protected BaseDataVM(TData data, Action saveCallback)
+        readonly Action? _saveCallback;
+        public event ChangeCallBack<TData>? Change;
+        public TData Data { get; }
+
+        protected BaseDataVM(TData data)
         {
-            _data = data ?? throw new ArgumentNullException(nameof(data));
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+        protected BaseDataVM(TData data, Action saveCallback) : this(data)
+        {
             this._saveCallback = saveCallback ?? throw new ArgumentNullException(nameof(saveCallback));
+        }
+
+        protected void Save()
+        {
+            _saveCallback?.Invoke();
+            Change?.Invoke(this, Data);
         }
     }
 }
