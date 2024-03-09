677b4099c33a6ed17996588970f69b9cbdc4e1a8
tqk2811 <tqk2811@gmail.com>
2024-03-09 20:26:20 +0700

    add OpenWindowCommand

---
+++ UI/ViewModels/Commands/OpenWindowCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace $safeprojectname$.UI.ViewModels.Commands
+{
+    internal class OpenWindowCommand : BaseCommand
+    {
+        readonly Func<Window> _func_window;
+        public OpenWindowCommand(Func<Window> func_window)
+        {
+            this._func_window = func_window ?? throw new ArgumentNullException(nameof(func_window));
+        }
+
+        public override void Execute(object? parameter)
+        {
+            this._func_window.Invoke().ShowDialog();
+        }
+    }
+}
