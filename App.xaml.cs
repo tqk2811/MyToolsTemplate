af331f2fedea100a8e9cd991af71014bf8eba6dc
tqk2811 <tqk2811@gmail.com>
2025-04-22 16:26:37 +0700

    auto close chromedrive and kill chrome on debug

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows;
 using System.Globalization;
 using TqkLibrary.WpfUi;
 using System.CommandLine;
+using TqkLibrary.WinApi.FindWindowHelper;
+using System.Diagnostics;
 using $safeprojectname$.UI;
 
 namespace $safeprojectname$
@@ -44,6 +46,34 @@ namespace $safeprojectname$
 
         private Task WorkAsync()
         {
+#if DEBUG
+            {
+                using Process? process = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "taskkill",
+                    Arguments = "/f /im chromedriver.exe",
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                });
+                process?.WaitForExit();
+            }
+
+            foreach (ProcessHelper processHelper in Process.GetProcessesByName("chrome").Select(x => new ProcessHelper((uint)x.Id)))
+            {
+                var Win32_Process = processHelper.Query_Win32_Process();
+                string? arguments = Win32_Process?.CommandLine;
+                if (arguments?.Contains(Singleton.UserDataDirs, StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    using Process? process = Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = "taskkill",
+                        Arguments = $"/f /pid {processHelper.ProcessId}",
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    });
+                }
+            }
+#endif
             Window window = new MainWindow();
             window.ShowDialog();
             return Task.CompletedTask;
