3b6dd5dc7e218d42626697a0a716022223bcf5ba
tqk2811 <tqk2811@gmail.com>
2025-08-28 15:54:27 +0700

    update

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Extensions.Logging;
+using TqkLibrary.WinApi.FindWindowHelper;
+using TqkLibrary.WinApi.WmiHelpers;
 using $safeprojectname$.UI.ViewModels;
 using $safeprojectname$.UI.ViewModels.WindowViewModels;
 
@@ -23,12 +25,45 @@ namespace $safeprojectname$.UI
     /// </summary>
     public partial class MainWindow : Window
     {
-		readonly MainWVM _mainWVM;
+        readonly MainWVM _mainWVM;
         readonly ILogger<MainWindow> _logger = Singleton.ILoggerFactory.CreateLogger<MainWindow>();
         public MainWindow()
         {
             InitializeComponent();
-			this._mainWVM = this.DataContext as MainWVM ?? throw new InvalidOperationException();
+            this._mainWVM = this.DataContext as MainWVM ?? throw new InvalidOperationException();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            {
+                using Process? process = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "taskkill",
+                    Arguments = "/f /im chromedriver.exe",
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                });
+                process?.WaitForExit();
+            }
+
+            foreach (ProcessHelper processHelper in Process.GetProcessesByName("chrome").Select(x => new ProcessHelper((uint)x.Id)))
+            {
+                Win32_Process? win32_Process = processHelper.Query_Win32_Process();
+                if (win32_Process?.CommandLine?.Contains(Singleton.UserDataDirs, StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    using Process? process = Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = "taskkill",
+                        Arguments = $"/f /pid {processHelper.ProcessId}",
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    });
+                }
+            }
+
+            MyBaseChromeProfile.KillChromeStuck();
+#endif
         }
     }
 }
